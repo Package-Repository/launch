453ae64f0f4c5d0f840622b58e4ea69d9d14b848
Mechatronics <mechatronics.sdsu@gmail.com>
2023-11-28 15:02:55 -0800

    added shared memory

--- launch.py
+++ launch.py
@@ -1,17 +1,52 @@
-import os
+from multiprocessing import Process, Value, set_start_method
 from vision.vision.vision_main import VideoRunner
+from shared_memory_reader import SharedMemoryReader
 #"/vision/vision/vision_main.py"
 def main():
+    # try:
+    #     set_start_method('spawn')
+    # except RuntimeError:
+    #     pass
     
-    processid = os.fork()
-
-    if processid == 0:
-        print("main process")
-    else:
-        print("child process")
-        vis = VideoRunner()
-        vis.run_loop()
-
+    #create shared memory
+    ang_vel_x = Value('d', 0.0)
+    ang_vel_y = Value('d', 0.0)
+    ang_vel_z = Value('d', 0.0)
+    lin_acc_x = Value('d', 0.0)
+    lin_acc_y = Value('d', 0.0)
+    lin_acc_z = Value('d', 0.0)
+    orientation_x = Value('d', 0.0)
+    orientation_y = Value('d', 0.0)
+    orientation_z = Value('d', 0.0)
+    depth = Value('d', 0.0)
+    offset_x = Value('d', 0.0)
+    offset_y = Value('d', 0.0)
+    
+    
+    vis = VideoRunner(linear_acceleration_x=lin_acc_x, linear_acceleration_y=lin_acc_y, linear_acceleration_z=lin_acc_z,        #linear accel x y z
+                        angular_velocity_x=ang_vel_x, angular_velocity_y=ang_vel_y, angular_velocity_z=ang_vel_z,               #angular velocity x y z
+                        orientation_x=orientation_x, orientation_y=orientation_y, orientation_z=orientation_z,                  #orientation x y z
+                        depth=depth,                                                                                            #depth
+                        offset_x=offset_x, offset_y=offset_y)                                                                   #offset x y
+    
+    shm = SharedMemoryReader(linear_acceleration_x=lin_acc_x, linear_acceleration_y=lin_acc_y, linear_acceleration_z=lin_acc_z, #linear accel x y z
+                        angular_velocity_x=ang_vel_x, angular_velocity_y=ang_vel_y, angular_velocity_z=ang_vel_z,               #angular velocity x y z
+                        orientation_x=orientation_x, orientation_y=orientation_y, orientation_z=orientation_z,                  #orientation x y z                 
+                        depth=depth,                                                                                            #depth
+                        offset_x=offset_x, offset_y=offset_y)                                                                   #offset x y
+    
+    #create processes
+    zed_process = Process(target=vis.run_loop)
+    reader_process = Process(target=shm.run_loop)
+    
+    # start processes
+    zed_process.start()
+    reader_process.start()
+    
+    # join processes
+    zed_process.join()
+    reader_process.join()
+    
 
 if __name__ == '__main__':
     main()
---
+++ shared_memory_reader.py
@@ -0,0 +1,38 @@
+from multiprocessing import Process, Value
+from subprocess import call
+
+class SharedMemoryReader:
+    def __init__(self, linear_acceleration_x , linear_acceleration_y, linear_acceleration_z, 
+                angular_velocity_x, angular_velocity_y, angular_velocity_z, 
+                orientation_x, orientation_y, orientation_z, 
+                depth,
+                offset_x, offset_y):
+        self.linear_acceleration_x = linear_acceleration_x
+        self.linear_acceleration_y = linear_acceleration_y
+        self.linear_acceleration_z = linear_acceleration_z
+        self.angular_velocity_x = angular_velocity_x
+        self.angular_velocity_y = angular_velocity_y
+        self.angular_velocity_z = angular_velocity_z
+        self.orientation_x = orientation_x
+        self.orientation_y = orientation_y
+        self.orientation_z = orientation_z
+        self.depth = depth
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+        
+    def run_loop(self):
+        while True:
+            print("linear_acceleration_x: ", self.linear_acceleration_x.value)
+            print("linear_acceleration_y: ", self.linear_acceleration_y.value)
+            print("linear_acceleration_z: ", self.linear_acceleration_z.value)
+            print("angular_velocity_x: ", self.angular_velocity_x.value)
+            print("angular_velocity_y: ", self.angular_velocity_y.value)
+            print("angular_velocity_z: ", self.angular_velocity_z.value)
+            print("orientation_x: ", self.orientation_x.value)
+            print("orientation_y: ", self.orientation_y.value)
+            print("orientation_z: ", self.orientation_z.value)
+            print("depth: ", self.depth.value)
+            print("offset_x: ", self.offset_x.value)
+            print("offset_y: ", self.offset_y.value)
+            print("--------------------------------------------------")
+            call('clear')
