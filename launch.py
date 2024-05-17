c8abdaa2cbde194e3f0c08e3e2d34dff26796e41
ayuyamo <doq300861@gmail.com>
2024-05-17 12:20:10 -0700

    add dvl values
    initialize dvl vaues & create dvl processes

--- launch.py
+++ launch.py
@@ -1,6 +1,7 @@
 from multiprocessing import Process, Value
 from vision.vision.vision_main import VideoRunner
 from shared_memory_reader import SharedMemoryReader
+from RobotAPI.dvl_cli import DVLData
 
 '''
     discord: @kialli
@@ -31,30 +32,42 @@ def main():
     offset_x = Value('d', 0.0)
     offset_y = Value('d', 0.0)
     
+    dvl_x = Value('d', 0.0)
+    dvl_y = Value('d', 0.0)    
+    dvl_z = Value('d', 0.0)    
+    dvl_yaw = Value('d', 0.0)    
+    dvl_pitch = Value('d', 0.0)    
+    dvl_roll = Value('d', 0.0) 
+    
+    dvl = DVLData(x=dvl_x, y=dvl_y, z=dvl_z, yaw=dvl_yaw, pitch=dvl_pitch, roll=dvl_roll)
     
     vis = VideoRunner(linear_acceleration_x=lin_acc_x, linear_acceleration_y=lin_acc_y, linear_acceleration_z=lin_acc_z,        #linear accel x y z
                         angular_velocity_x=ang_vel_x, angular_velocity_y=ang_vel_y, angular_velocity_z=ang_vel_z,               #angular velocity x y z
                         orientation_x=orientation_x, orientation_y=orientation_y, orientation_z=orientation_z,                  #orientation x y z
                         depth=depth,                                                                                            #depth
                         offset_x=offset_x, offset_y=offset_y)                                                                   #offset x y
+
     
     shm = SharedMemoryReader(linear_acceleration_x=lin_acc_x, linear_acceleration_y=lin_acc_y, linear_acceleration_z=lin_acc_z, #linear accel x y z
                         angular_velocity_x=ang_vel_x, angular_velocity_y=ang_vel_y, angular_velocity_z=ang_vel_z,               #angular velocity x y z
                         orientation_x=orientation_x, orientation_y=orientation_y, orientation_z=orientation_z,                  #orientation x y z                 
                         depth=depth,                                                                                            #depth
-                        offset_x=offset_x, offset_y=offset_y)                                                                   #offset x y
+                        offset_x=offset_x, offset_y=offset_y)                                                                 #offset x y
     
     #create processes
     zed_process = Process(target=vis.run_loop)
     reader_process = Process(target=shm.run_loop)
+    dvl_process = Process(target=dvl.run_loop)
     
     # start processes
     zed_process.start()
     reader_process.start()
+    dvl_process.start()
     
     # join processes
     zed_process.join()
     reader_process.join()
+    dvl_process.join()
     
 
 if __name__ == '__main__':
