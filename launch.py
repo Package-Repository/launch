229f45788e521e8f95a71d016964f33225863189
kchan5071 <kchan5071@sdsu.edu>
2024-07-17 20:50:05 -0700

    update from orin

--- launch.py
+++ launch.py
@@ -1,7 +1,8 @@
-from multiprocessing import Process, Value
-from vision.vision.vision_main import VideoRunner
-from shared_memory_reader import SharedMemoryReader
-# from RobotAPI.dvl_cli import DVLData
+from multiprocessing                import Process, Value
+from vision.vision_main             import VideoRunner
+from shared_memory_reader           import SharedMemoryReader
+from sensors.depth_sensor_interface import DepthSensorInterface
+from motors.MotorInterface          import MotorInterface
 
 '''
     discord: @kialli
@@ -16,58 +17,86 @@ from shared_memory_reader import SharedMemoryReader
     
 '''
 
-
-
 def main():
-    ang_vel_x = Value('d', 0.0)
-    ang_vel_y = Value('d', 0.0)
-    ang_vel_z = Value('d', 0.0)
-    lin_acc_x = Value('d', 0.0)
-    lin_acc_y = Value('d', 0.0)
-    lin_acc_z = Value('d', 0.0)
-    orientation_x = Value('d', 0.0)
-    orientation_y = Value('d', 0.0)
-    orientation_z = Value('d', 0.0)
-    depth = Value('d', 0.0)
-    offset_x = Value('d', 0.0)
-    offset_y = Value('d', 0.0)
+    ang_vel_x                   = Value('d', 0.0)
+    ang_vel_y                   = Value('d', 0.0)
+    ang_vel_z                   = Value('d', 0.0)
+    lin_acc_x                   = Value('d', 0.0)
+    lin_acc_y                   = Value('d', 0.0)
+    lin_acc_z                   = Value('d', 0.0)
+    orientation_x               = Value('d', 0.0)
+    orientation_y               = Value('d', 0.0)
+    orientation_z               = Value('d', 0.0)
+    depth                       = Value('d', 0.0)
+    offset_x                    = Value('d', 0.0)
+    offset_y                    = Value('d', 0.0)
+    depth_z                     = Value('d', 0.0)    
     
-    dvl_x = Value('d', 0.0)
-    dvl_y = Value('d', 0.0)    
-    dvl_z = Value('d', 0.0)    
-    dvl_yaw = Value('d', 0.0)    
-    dvl_pitch = Value('d', 0.0)    
-    dvl_roll = Value('d', 0.0) 
+    depth_sensor = DepthSensorInterface(z=depth_z)
     
-    #dvl = DVLData(x=dvl_x, y=dvl_y, z=dvl_z, yaw=dvl_yaw, pitch=dvl_pitch, roll=dvl_roll)
-    
-    vis = VideoRunner(linear_acceleration_x=lin_acc_x, linear_acceleration_y=lin_acc_y, linear_acceleration_z=lin_acc_z,        #linear accel x y z
-                        angular_velocity_x=ang_vel_x, angular_velocity_y=ang_vel_y, angular_velocity_z=ang_vel_z,               #angular velocity x y z
-                        orientation_x=orientation_x, orientation_y=orientation_y, orientation_z=orientation_z,                  #orientation x y z
-                        depth=depth,                                                                                            #depth
-                        offset_x=offset_x, offset_y=offset_y)                                                                   #offset x y
+    vis = VideoRunner(
+        linear_acceleration_x   = lin_acc_x,
+        linear_acceleration_y   = lin_acc_y,
+        linear_acceleration_z   = lin_acc_z,
+        angular_velocity_x      = ang_vel_x,
+        angular_velocity_y      = ang_vel_y,
+        angular_velocity_z      = ang_vel_z,
+        orientation_x           = orientation_x,
+        orientation_y           = orientation_y,
+        orientation_z           = orientation_z,
+        depth                   = depth,
+        offset_x                = offset_x,
+        offset_y                = offset_y
+    )
 
+    interface = MotorInterface(
+        linear_acceleration_x   = lin_acc_x,
+        linear_acceleration_y   = lin_acc_y,
+        linear_acceleration_z   = lin_acc_z,
+        angular_velocity_x      = ang_vel_x,
+        angular_velocity_y      = ang_vel_y,
+        angular_velocity_z      = ang_vel_z,
+        orientation_x           = orientation_x,
+        orientation_y           = orientation_y,
+        orientation_z           = orientation_z,
+        depth                   = depth,
+        offset_x                = offset_x,
+        offset_y                = offset_y,
+        dvl_z                   = depth_z
+    )       
     
-    shm = SharedMemoryReader(linear_acceleration_x=lin_acc_x, linear_acceleration_y=lin_acc_y, linear_acceleration_z=lin_acc_z, #linear accel x y z
-                        angular_velocity_x=ang_vel_x, angular_velocity_y=ang_vel_y, angular_velocity_z=ang_vel_z,               #angular velocity x y z
-                        orientation_x=orientation_x, orientation_y=orientation_y, orientation_z=orientation_z,                  #orientation x y z                 
-                        depth=depth,                                                                                            #depth
-                        offset_x=offset_x, offset_y=offset_y)                                                                 #offset x y
+    shm = SharedMemoryReader(
+        linear_acceleration_x   = lin_acc_x,
+        linear_acceleration_y   = lin_acc_y,
+        linear_acceleration_z   = lin_acc_z,
+        angular_velocity_x      = ang_vel_x,
+        angular_velocity_y      = ang_vel_y,
+        angular_velocity_z      = ang_vel_z,
+        orientation_x           = orientation_x,
+        orientation_y           = orientation_y,
+        orientation_z           = orientation_z,             
+        depth                   = depth,
+        offset_x                = offset_x,
+        offset_y                = offset_y
+    )
     
     #create processes
-    zed_process = Process(target=vis.run_loop)
-    reader_process = Process(target=shm.run_loop)
-    #dvl_process = Process(target=dvl.run_loop)
+    zed_process                 = Process(target=vis.run_loop)
+    reader_process              = Process(target=shm.run_loop)
+    depth_sensor_process        = Process(target=depth_sensor.run_loop)
+    interface                   = Process(target=interface.run_loop)
     
     # start processes
     zed_process.start()
     reader_process.start()
-    #dvl_process.start()
+    depth_sensor.start()
+    interface.start()
     
     # join processes
     zed_process.join()
     reader_process.join()
-    #dvl_process.join()
+    depth_sensor.join()
+    interface.join()
     
 
 if __name__ == '__main__':
---
+++ read_can.py
@@ -0,0 +1,43 @@
+import can
+import time
+from subprocess import run, call
+from multiprocessing import Process, Value
+from launch import main as launch
+
+
+filters = [{"can_id": 0x007, "can_mask": 0x7FF}]
+bus = can.Bus(interface='socketcan',channel = 'can0', receive_own_messages=True, can_filters = filters)
+
+launch_process = Process(target=launch)
+
+#launch_arbitration_ID =  0x007
+
+        #if msg.arbitration_id == launch_arbitration_ID:
+while True:
+    message = bus.recv()
+    if message == None:
+        continue
+    data = message.data
+    if data[0] == None:
+        continue
+    if message.arbitration_id == 0x007:
+        if data[0] == 4:
+            message = can.Message(arbitration_id = 10, is_extended_id = False, data = None)
+            bus.send(message)
+            launch_process.start()
+            #run(["python3", "launch.py"])
+            light_enable = bytearray([0x04, 0x00, 0x00, 0x00, 0x01])
+            message = can.Message(arbitration_id = 34, is_extended_id = False, data = light_enable)
+            bus.send(message)
+            light_on = bytearray([0x04, 0x00, 0x04, 0x00, 0x32])
+            message = can.Message(arbitration_id = 34, is_extended_id = False, data = light_on)
+            bus.send(message)
+            time.sleep(10)
+            light_off = bytearray([0x04, 0x00, 0x04, 0x00, 0x00])
+            message = can.Message(arbitration_id = 34, is_extended_id = False, data = light_off)
+            bus.send(message)
+        else:
+            print("it didnt work bitch")
+    # filter.filter
+    #arbitration ID ytes()
+    #filter out any command thats not 007
