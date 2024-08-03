afbefddd26ddf0e46f7312586258235018a90434
mechatronics <kchan5071@sdsu.edu>
2024-08-03 15:34:43 -0700

    fixes

--- launch.py
+++ launch.py
@@ -1,10 +1,12 @@
-from multiprocessing                    import Process, Value
-# from vision.vision_main                 import VideoRunner
-from shared_memory_reader               import SharedMemoryReader
-from sensors.depth_sensor_interface     import DepthSensorInterface
-from motors.MotorInterface              import MotorInterface
-# import ctypes
-from vision.ColorFilter.color_filter    import ColorFilter
+from multiprocessing                import Process, Value
+from vision.vision_main             import VideoRunner
+from shared_memory_reader           import SharedMemoryReader
+from sensors.depth_sensor_interface import DepthSensorInterface
+from motors.MotorInterface          import MotorInterface
+from kill_button_interface          import Kill_Button_Interface
+import ctypes
+
+import time
 
 """
     discord: @kialli
@@ -18,7 +20,6 @@ from vision.ColorFilter.color_filter    import ColorFilter
     could use arrays, too lazy
     
 """
-
 def main():
     ang_vel_x                   = Value('d', 0.0)
     ang_vel_y                   = Value('d', 0.0)
@@ -28,7 +29,7 @@ def main():
     lin_acc_z                   = Value('d', 0.0)
     orientation_x               = Value('d', 0.0)
     orientation_y               = Value('d', 0.0)
-    orientation_z               = Value('d', 0.0)
+    orientation_z               = Value('d', 0.0)   
     distance                    = Value('d', 0.0)
     yolo_offset_x               = Value('d', 0.0)
     yolo_offset_y               = Value('d', 0.0)
@@ -37,35 +38,37 @@ def main():
     color_offset_x              = Value('d', 0.0)
     color_offset_y              = Value('d', 0.0)
     color_enable                = Value('b', False)
-    yolo_enable                 = Value('b', False)
+    yolo_enable                 = Value('b', True)
+    running                     = Value('b', True)
 
-    
 
 
-    
-    depth_sensor = DepthSensorInterface(z=depth_z)
 
-    color_sensor = ColorFilter(color_offset_x, color_offset_y)
-    
-    # vis = VideoRunner(
-    #     linear_acceleration_x   = lin_acc_x,
-    #     linear_acceleration_y   = lin_acc_y,
-    #     linear_acceleration_z   = lin_acc_z,
-    #     angular_velocity_x      = ang_vel_x,
-    #     angular_velocity_y      = ang_vel_y,
-    #     angular_velocity_z      = ang_vel_z,
-    #     orientation_x           = orientation_x,
-    #     orientation_y           = orientation_y,
-    #     orientation_z           = orientation_z,
-    #     distance                = distance,
-    #     yolo_offset_x           = yolo_offset_x,
-    #     yolo_offset_y           = yolo_offset_y,
-    #     color                   = color,
-    #     color_offset_x          = color_offset_x,
-    #     color_offset_y          = color_offset_y,
-    #     color_enable            = color_enable,
-    #     yolo_enable             = yolo_enable
-    # )
+
+    depth_sensor = DepthSensorInterface(z=depth_z, running = running)
+
+    kill_button_listener = Kill_Button_Interface(running=running)
+
+    vis = VideoRunner(
+        linear_acceleration_x   = lin_acc_x,
+        linear_acceleration_y   = lin_acc_y,
+        linear_acceleration_z   = lin_acc_z,
+        angular_velocity_x      = ang_vel_x,
+        angular_velocity_y      = ang_vel_y,
+        angular_velocity_z      = ang_vel_z,
+        orientation_x           = orientation_x,
+        orientation_y           = orientation_y,
+        orientation_z           = orientation_z,
+        distance                = distance,
+        yolo_offset_x           = yolo_offset_x,
+        yolo_offset_y           = yolo_offset_y,
+        color                   = color,
+        color_offset_x          = color_offset_x,
+        color_offset_y          = color_offset_y,
+        color_enable            = color_enable,
+        yolo_enable             = yolo_enable,
+        running                 = running
+    )
 
     interface = MotorInterface(
         linear_acceleration_x   = lin_acc_x,
@@ -77,51 +80,53 @@ def main():
         orientation_x           = orientation_x,
         orientation_y           = orientation_y,
         orientation_z           = orientation_z,
-        depth                   = distance,
-        # yolo_offset_x           = yolo_offset_x,
-        # yolo_offset_y           = yolo_offset_y,
         dvl_z                   = depth_z,
-        offset_x                = color_offset_x,
-        offset_y                = color_offset_y
+        distance                = distance,
+        yolo_offset_x           = yolo_offset_x,
+        yolo_offset_y           = yolo_offset_y,
+        color_offset_x          = color_offset_x,
+        color_offset_y          = color_offset_y,
+        running                 = running,
+        enable_color            = color_enable,
+        enable_yolo             = yolo_enable
     )       
-    
-    shm = SharedMemoryReader(
-        linear_acceleration_x   = lin_acc_x,
-        linear_acceleration_y   = lin_acc_y,
-        linear_acceleration_z   = lin_acc_z,
-        angular_velocity_x      = ang_vel_x,
-        angular_velocity_y      = ang_vel_y,
-        angular_velocity_z      = ang_vel_z,
-        orientation_x           = orientation_x,
-        orientation_y           = orientation_y,
-        orientation_z           = orientation_z,             
-        depth                   = distance,
-        offset_x                = color_offset_x,
-        offset_y                = color_offset_y
-    )
-    
-    #create processes
-    # zed_process                 = Process(target=vis.run_loop)
-    # reader_process              = Process(target=shm.run_loop)
-    depth_sensor_process        = Process(target=depth_sensor.run_loop)
-    interface                   = Process(target=interface.run_loop)
-    detect_color                = Process(target=color_sensor.set_offsets_loop)
 
+    # shm = SharedMemoryReader(
+    #     linear_acceleration_x   = lin_acc_x,
+    #     linear_acceleration_y   = lin_acc_y,
+    #     linear_acceleration_z   = lin_acc_z,
+    #     angular_velocity_x      = ang_vel_x,
+    #     angular_velocity_y      = ang_vel_y,
+    #     angular_velocity_z      = ang_vel_z,
+    #     orientation_x           = orientation_x,
+    #     orientation_y           = orientation_y,
+    #     orientation_z           = orientation_z,             
+    #     yolo_offset_x           = yolo_offset_x,
+    #     yolo_offset_y           = yolo_offset_y
+    # )
+
+    #create processes
+    zed_process                 = Process(target=vis.run_loop)
+    # reader_process              = Process(target=shm.run_loop)
+    kill_button_listener_process = Process(target=kill_button_listener.run_loop)
+    depth_sensor_process        = Process(target=depth_sensor.run_loop)
+
+    interface                   = Process(target=interface.run_loop)
     
     # start processes
-    # zed_process.start()
-    #reader_process.start()
+    zed_process.start()
+    # reader_process.start()
+    kill_button_listener_process.start()
     depth_sensor_process.start()
     interface.start()
-    detect_color.start()
     
     # join processes
-    # zed_process.join()
-    #reader_process.join()
+    zed_process.join()
+    # reader_process.join()
+    kill_button_listener_process.join()
     depth_sensor_process.join()
     interface.join()
-    detect_color.join()
-    
 
 if __name__ == '__main__':
+    print("RAW FROM LAUNCH")
     main()
--- read_can.py
+++ read_can.py
@@ -8,12 +8,11 @@ from launch import main as launch
 filters = [{"can_id": 0x007, "can_mask": 0x7FF}]
 bus = can.Bus(interface='socketcan',channel = 'can0', receive_own_messages=True, can_filters = filters)
 
-launch_process = Process(target=launch)
-
 #launch_arbitration_ID =  0x007
 
         #if msg.arbitration_id == launch_arbitration_ID:
 while True:
+    time.sleep(.1)
     message = bus.recv()
     if message == None:
         continue
@@ -22,10 +21,13 @@ while True:
         continue
     if message.arbitration_id == 0x007:
         if data[0] == 4:
+            print("STARTING", data[0])
+            time.sleep(3)
             message = can.Message(arbitration_id = 10, is_extended_id = False, data = None)
             bus.send(message)
+            launch_process = Process(target=launch)
             launch_process.start()
-            #run(["python3", "launch.py"])
+            launch_process.join()
             light_enable = bytearray([0x04, 0x00, 0x00, 0x00, 0x01])
             message = can.Message(arbitration_id = 34, is_extended_id = False, data = light_enable)
             bus.send(message)
@@ -36,6 +38,12 @@ while True:
             light_off = bytearray([0x04, 0x00, 0x04, 0x00, 0x00])
             message = can.Message(arbitration_id = 34, is_extended_id = False, data = light_off)
             bus.send(message)
+        elif data[0] == 0:
+            try:
+                time.sleep(3)
+                print("READY")
+            except:
+                pass
         else:
             print("it didnt work bitch")
     # filter.filter
