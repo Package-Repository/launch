0242ed123a3833ef5ff86bda70a35c78760fd895
kchan5071 <kchan5071@sdsu.edu>
2024-09-06 18:07:31 -0700

    simplify shared_memory_wrapper

--- shared_memory_wrapper.py
+++ shared_memory_wrapper.py
@@ -3,16 +3,6 @@ from multiprocessing import Value, Array
 
 class SharedMemoryWrapper:
     def __init__(self):
-        self.imu_lin_acc            = Array('d', 3)
-        self.imu_ang_vel            = Array('d', 3) #pitch yaw roll
-        self.imu_orientation        = Array('d', 3)
-        self.distance_from_object   = Value('d', 0)
-        self.yolo_offset            = Array('d', 2)
-        self.color_offset           = Array('d', 2)
-        self.gate_offset            = Array('d', 2)
-        self.color_enable           = Value('b', False)
-        self.yolo_enable            = Value('b', False)
-        self.gate_enable            = Value('b', False)
-        self.current_color_index    = Value('i', 0)
-        self.depth                  = Value('d', 0) 
+        #add properties here
+        self.example_array          = Array('i', [1, 2, 3, 4, 5])
         self.running                = Value('i', 1)
