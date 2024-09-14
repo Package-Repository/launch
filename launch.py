a090a937cd1036a595dc4fa42b8a03b6e082b1f9
kchan5071 <kchan5071@sdsu.edu>
2024-09-13 20:05:14 -0700

    dvl works inside tub

--- launch.py
+++ launch.py
@@ -1,7 +1,7 @@
 from multiprocessing                import Process, Value
 from example_process                import Example_Process
-from kill_button_interface          import Kill_Button_Interface
 from shared_memory_wrapper          import SharedMemoryWrapper
+from sensors.a50_dvl.dvl_interface  import DVL_Interface
 
 """
     discord: @kialli
@@ -16,26 +16,26 @@ def main():
     shared_memory_object = SharedMemoryWrapper()
 
     # create objects
-    kill_button_listener = Kill_Button_Interface(running = shared_memory_object.running)
     example_object = Example_Process(shared_memory_object) 
+    dvl_object = DVL_Interface(shared_memory_object)
 
     #ADD OBJECTS HERE   
 
     #create processes
-    kill_button_listener_process = Process(target=kill_button_listener.run_loop)
     example_process = Process(target=example_object.run_loop)
+    dvl_process = Process(target=dvl_object.run_loop)
 
     #ADD PROCESSES HERE
     
     # start processes
-    kill_button_listener_process.start()
     example_process.start()
+    dvl_process.start()
 
     #ADD START PROCESSES HERE
 
     # wait for processes to finish
-    kill_button_listener_process.join()
     example_process.join()
+    dvl_process.join()
 
     #ADD JOIN PROCESSES HERE
 
